1e613825066c431938636f3c9376ebeef7ce39ee
Erwi <erwi@outlook.com>
2017-07-18 16:19:14 +0300

    add user

---
+++ src/main/java/com/exdev/italent/model/User.java
@@ -0,0 +1,112 @@
+package com.exdev.italent.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * The persistent class for the user database table.
+ * 
+ */
+@Entity
+@Table(name = "user")
+@NamedQueries({ @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u"),
+	@NamedQuery(name = "User.findByToken", query = "SELECT u FROM User u where u.token = :token"),
+	@NamedQuery(name = "User.findByMobileNo", query = "SELECT u FROM User u where u.mobileNo = :mobileNo"),
+	@NamedQuery(name = "User.findByUUID", query = "SELECT u FROM User u where u.uuid = :uuid")})
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private boolean disabled;
+	private String name;
+	private String notes;
+
+	private String password;
+	
+	@Column(name = "mobile_no")
+	private String mobileNo;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "login_date")
+	private Date loginDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "logout_date")
+	private Date logoutDate;
+	
+	private String token;
+	private String uuid;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public boolean isDisabled() {
+		return disabled;
+	}
+	public void setDisabled(boolean disabled) {
+		this.disabled = disabled;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	public Date getLoginDate() {
+		return loginDate;
+	}
+	public void setLoginDate(Date loginDate) {
+		this.loginDate = loginDate;
+	}
+	public Date getLogoutDate() {
+		return logoutDate;
+	}
+	public void setLogoutDate(Date logoutDate) {
+		this.logoutDate = logoutDate;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+}
