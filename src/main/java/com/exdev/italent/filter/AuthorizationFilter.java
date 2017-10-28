e4a4f83963921121649c38ba278e40eaa9ba47bc
erwi <erwi@erwi-hp>
2017-10-29 00:14:15 +0300

    add auth

---
+++ src/main/java/com/exdev/italent/filter/AuthorizationFilter.java
@@ -0,0 +1,58 @@
+package com.exdev.italent.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class AuthorizationFilter
+ */
+@WebFilter("/*")
+public final class AuthorizationFilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public AuthorizationFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+
+		String authKeyServer = request.getServletContext().getInitParameter("AUTH_KEY");
+		String authKeyClient = request.getParameter("AUTH_KEY");
+
+		if (!authKeyServer.equals(authKeyClient))
+			throw new ServletException("AUTH_KEY INVALID");
+		else
+			chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
